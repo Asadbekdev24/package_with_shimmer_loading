916ee340f34bd5b29f404aedfaca4c0cf67d7238
Asadbekdev24 <101445735+Asadbekdev24@users.noreply.github.com>
2025-03-17 15:16:28 +0500

    boshlab oldik

---
+++ example/main.dart
@@ -0,0 +1,9 @@
+import 'package:my_package_with_shimmer/my_package_with_shimmer.dart';
+
+void main(List<String> args) {
+
+  final calcFunk=Calculator();
+
+  print(calcFunk.addOne(1));
+  print(calcFunk.addTwo(2, 3));
+}
